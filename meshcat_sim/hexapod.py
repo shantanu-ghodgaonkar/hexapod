72d19c1c6f11417ae9c769a03d3c8187b6f4d678
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-09-17 10:41:54 -0400

    Comments added

--- meshcat_sim/hexapod.py
+++ meshcat_sim/hexapod.py
@@ -13,31 +13,50 @@ np.set_printoptions(suppress=True, precision=4)
 
 
 class hexapod:
+    """The hexapod class represents a virtual representation of the hexapod that is being built as a part of our Master's Capstone Project.
+    """
 
     def __init__(self, init_viz=True) -> None:
+        """Default constructor
+
+        Args:
+            init_viz (bool, optional): Choose whether or not to initialise a meshcat visualiser
+        """
+        # load URDF directory path
         self.pin_model_dir = str(
             Path('URDF/URDF4Pin').absolute())
+        # load URDF file path
         self.urdf_filename = (self.pin_model_dir + '/URDF4Pin.urdf')
-
+        # load robot model
         self.robot = pin.RobotWrapper.BuildFromURDF(
             self.urdf_filename, self.pin_model_dir, root_joint=pin.JointModelFreeFlyer())
-
+        # initialise joint state
         self.robot.forwardKinematics(pin.neutral(self.robot.model))
+        # update visual_model placements
         self.robot.updateGeometryPlacements()
-        self.direction_slope = 0.0
+        # set half of the step size of each foot in XY plane
         self.HALF_STEP_SIZE_XY = 0.06/2
+        # set the max height at which each leg can go while walking
         self.STEP_SIZE_Z = 0.025
+        # array to store joint IDs of all the feet
         self.FOOT_IDS = [self.robot.model.getFrameId(
             frame.name) for frame in self.robot.model.frames if "foot" in frame.name]
+        # array to store shoulder IDs of all the feet
         self.SHOULDER_IDS = [self.robot.model.getFrameId(
             frame.name) for frame in self.robot.model.frames if "Revolute_joint_0" in frame.name]
+        # Flag to store the state of motion
         self.state_flag = 'START'
+        # qc stores the current joint state of the robot to neutral
         self.qc = self.robot.q0
+        # flag to store if visualiser is needed or not
         self.viz_flag = init_viz
         if self.viz_flag == True:
+            # if true, initialise the meshcat visualiser
             self.init_viz()
 
     def init_viz(self):
+        """Function to initialise the visualiser and assign it to a data member
+        """
         try:
             self.viz = pin.visualize.MeshcatVisualizer(
                 self.robot.model, self.robot.collision_model, self.robot.visual_model)
@@ -56,29 +75,52 @@ class hexapod:
         self.viz.displayCollisions(visibility=False)
         self.viz.display(self.qc)
 
-    def foot_pos_err(self, q, FRAME_ID=9, desired_pos=np.zeros(3)):
+    def foot_pos_err(self, q, FRAME_ID=10, desired_pos=np.zeros(3)):
+        """Function to obtain the error in the foot position (i.e. (current - desired)**2)
+
+        Args:
+            q (_type_): Joint state matrix
+            FRAME_ID (int, optional): Foot ID of chosen foot. Defaults to 10.
+            desired_pos (_type_, optional): Desired position of chosen foot. Defaults to np.zeros(3).
+
+        Returns:
+            _type_: error in foot position, squared
+        """
         self.robot.forwardKinematics(q)
-        # current_pos = self.robot.visual_model.geometryObjects.tolist()[
-        #     (FOOT) * 4].placement.translation
         current_pos = self.robot.framePlacement(q, FRAME_ID).translation
-        # current_pos = (np.concatenate(
-        #     (current_pos, [1])) * self.FOOT1HT)[0:3, 3]
         error = current_pos - desired_pos
         return error.dot(error)
 
     def print_all_frame_info(self):
+        """Print all the frame names, IDs and positions
+        """
         for frame in self.robot.model.frames:
             # Print the frame name and type
             print(
                 f"Frame Name: {frame.name}, Frame Type: {frame.type}, Frame ID: {self.robot.model.getFrameId(frame.name)}, XYZ = {self.robot.framePlacement(self.qc, self.robot.model.getFrameId(frame.name))}")
 
     def find_frame_info_by_name(self, name: str):
+        """Print only those frames whose names contain matching string
+
+        Args:
+            name (str): Name matching string
+        """
         for frame in self.robot.model.frames:
             if name in frame.name:
                 print(
                     f"Frame Name: {frame.name}, Frame Type: {frame.type}, Frame ID: {self.robot.model.getFrameId(frame.name)}, XYZ = {self.robot.framePlacement(self.qc, self.robot.model.getFrameId(frame.name))}")
 
-    def inverse_geometery(self, q, FRAME_ID=9, desired_pos=np.zeros(3)):
+    def inverse_geometery(self, q, FRAME_ID=10, desired_pos=np.zeros(3)):
+        """Inverse geometry computation
+
+        Args:
+            q (_type_): Starting point for minimisation function (Initial Joint State)
+            FRAME_ID (int, optional): End effector frame ID. Defaults to 10.
+            desired_pos (_type_, optional): Desired position of end effector. Defaults to np.zeros(3).
+
+        Returns:
+            _type_: Joint state corresponding to desired position
+        """
         bounds = [(0., 0.)]*self.robot.nq
         bounds[0] = (0, 0)
         bounds[1] = (0, 0)
@@ -94,6 +136,11 @@ class hexapod:
         return res.x
 
     def north_vector(self):
+        """Computes the vector pointing North of the robot
+
+        Returns:
+            _type_: Vector pointing North
+        """
         # Get frame of Joint_01
         p1 = self.robot.framePlacement(
             self.qc, self.SHOULDER_IDS[0]).translation
@@ -110,6 +157,11 @@ class hexapod:
         return v
 
     def south_vector(self):
+        """Computes the vector pointing South of the robot
+
+        Returns:
+            _type_: Vector pointing South
+        """
         # Get frame of Joint_01
         p1 = self.robot.framePlacement(
             self.qc, self.SHOULDER_IDS[3]).translation
@@ -148,6 +200,14 @@ class hexapod:
             "NOT A VAILD DIRECTION. CHOOSE ONE OF {N, S, E, W, NE, NW, SE, SW} ONLY")
 
     def generate_direction_vector(self, DIR='N'):
+        """Generates the vector pointing in the chosen direction
+
+        Args:
+            DIR (str, optional): Chosen direction. Accepted values {N, S, E, W, NE, NW, SE, SW}. Defaults to 'N'.
+
+        Returns:
+            _type_: 2D vector in XY plane that points in the chosen direction
+        """
         slope_switch_dict = {
             'N': self.north_vector,
             'S': self.south_vector,
@@ -161,6 +221,13 @@ class hexapod:
         return slope_switch_dict.get(DIR, self.default_vector)()
 
     def init_foot_trajectory_functions(self, step_size_xy_mult, DIR='N', FOOT_ID=10):
+        """Function to initialise the parabolic trajectory of a foot
+
+        Args:
+            step_size_xy_mult (_type_): Multiplier to have either half or full step size
+            DIR (str, optional): Chosen direction of motion. Defaults to 'N'
+            FOOT_ID (int, optional): Chosen foot frame ID. Defaults to 10
+        """
         v = self.generate_direction_vector(DIR=DIR)
         p1 = self.robot.framePlacement(self.qc, FOOT_ID).translation[0:2]
         p2 = p1 + (self.HALF_STEP_SIZE_XY * step_size_xy_mult * v)
@@ -172,6 +239,17 @@ class hexapod:
         pass
 
     def generate_joint_waypoints(self, step_size_xy_mult, STEPS=5, DIR='N', FOOT_ID=10):
+        """Function to generate waypoints along the generated parabolic trajectory of the foot
+
+        Args:
+            step_size_xy_mult (_type_): Multiplier to have either half or full step size
+            STEPS (int, optional): Number of waypoints along the trajectory. Defaults to 5.
+            DIR (str, optional): Chosen direction of motion. Defaults to 'N'
+            FOOT_ID (int, optional): Chosen foot frame ID. Defaults to 10
+
+        Returns:
+            _type_: Array of joint spaces, where each element corresponds to a computed waypoint along the parabolic trajectory of the foot
+        """
         self.init_foot_trajectory_functions(
             step_size_xy_mult=step_size_xy_mult, DIR=DIR, FOOT_ID=FOOT_ID)
         s = np.linspace(0, 1, STEPS)
@@ -187,6 +265,18 @@ class hexapod:
                 for wp in waypoints]
 
     def compute_trajectory(self, position_init, position_goal, t_init, t_goal, t):
+        """Time parameterisation of the trajectory with acceleation and velocity constraints 
+
+        Args:
+            position_init (_type_): Initial position in Joint state form
+            position_goal (_type_): Goal position in Joint state form
+            t_init (_type_): Initial time
+            t_goal (_type_): Goal time
+            t (_type_): Current time
+
+        Returns:
+            _type_: For time t : desired position in Joint state, desired velocity as a differential of Joint state, desired acceleration as a double differential of Joint state
+        """
         t_tot = t_goal - t_init
         self.desired_position = position_init + (
             ((10 / (t_tot**3)) * ((t - t_init)**3))
@@ -204,6 +294,20 @@ class hexapod:
         return self.desired_position, self.desired_acceleration, self.desired_acceleration
 
     def generate_leg_joint_trajectory(self, step_size_xy_mult, DIR='N', LEG=0, STEPS=5, t_init=0, t_goal=0.1, dt=0.01):
+        """Fucntion to generate the entire trajectory of a chosen foot, from current position to goal position, in Joint state form
+
+        Args:
+            step_size_xy_mult (_type_): Choice between half (1) or full (2) step size. Possible vales {1, 2}
+            DIR (str, optional): Chosen direction of motion. Defaults to 'N'.
+            LEG (int, optional): Chosen leg. Defaults to 0.
+            STEPS (int, optional): Number of waypoints required between current and goal postion. Defaults to 5.
+            t_init (int, optional): Initial time. Defaults to 0.
+            t_goal (float, optional): Goal time. Defaults to 0.1.
+            dt (float, optional): Time divisions between t_init and t_goal. Defaults to 0.01.
+
+        Returns:
+            _type_: A 2D array, where each row forms a Joint state for the leg in its motion along the chosen path
+        """
         q_wps = self.generate_joint_waypoints(step_size_xy_mult,
                                               DIR=DIR, FOOT_ID=self.FOOT_IDS[LEG], STEPS=STEPS)
         q_traj = self.qc

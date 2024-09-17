c6b5e45b69319ef0cd8d8875c99f2cca370c0fff
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-09-16 22:37:21 -0400

    1. Joint names in updated in URDF 2. Foot trajectory logic moved from main to hexapod class

--- meshcat_sim/hexapod.py
+++ meshcat_sim/hexapod.py
@@ -2,14 +2,10 @@
 from time import sleep, time
 import numpy as np
 import pinocchio as pin
-import os
-import meshcat
-import matplotlib as mp
-import matplotlib.pyplot as plt
+import meshcat.geometry as g
 import scipy
 from pathlib import Path
 import sys
-
 import scipy.optimize
 
 # we don't want to print every decimal!
@@ -18,17 +14,10 @@ np.set_printoptions(suppress=True, precision=4)
 
 class hexapod:
 
-    def __init__(self) -> None:
+    def __init__(self, init_viz=True) -> None:
         self.pin_model_dir = str(
             Path('URDF/URDF4Pin').absolute())
-        # self.pin_model_dir = str(
-        #     Path('/home/shantanu/Documents/hexapod_files/CS_Hexapod_Ex_1').absolute())
-        # self.pin_model_dir = str(Path('re2').absolute())
-
-        # You should change here to set up your own URDF file or just pass it as an argument of this example.
         self.urdf_filename = (self.pin_model_dir + '/URDF4Pin.urdf')
-        # self.urdf_filename = (self.pin_model_dir + '/CS_Hexapod_Ex_1.urdf')
-        # self.urdf_filename = (self.pin_model_dir + '/IKTRials_redone naming convention.urdf')
 
         self.robot = pin.RobotWrapper.BuildFromURDF(
             self.urdf_filename, self.pin_model_dir, root_joint=pin.JointModelFreeFlyer())
@@ -36,24 +25,36 @@ class hexapod:
         self.robot.forwardKinematics(pin.neutral(self.robot.model))
         self.robot.updateGeometryPlacements()
         self.direction_slope = 0.0
-        self.STEP_SIZE_XY = 0.05
+        self.HALF_STEP_SIZE_XY = 0.06/2
         self.STEP_SIZE_Z = 0.025
         self.FOOT_IDS = [self.robot.model.getFrameId(
             frame.name) for frame in self.robot.model.frames if "foot" in frame.name]
         self.SHOULDER_IDS = [self.robot.model.getFrameId(
             frame.name) for frame in self.robot.model.frames if "Revolute_joint_0" in frame.name]
         self.state_flag = 'START'
-        self.FOOT1HT = self.robot.framePlacement(self.robot.q0, 9)
-        # self.print_all_IDs()
+        self.qc = self.robot.q0
+        self.viz_flag = init_viz
+        if self.viz_flag == True:
+            self.init_viz()
 
-        # self.robot.model.addFrame(pin.Frame())
+    def init_viz(self):
+        try:
+            self.viz = pin.visualize.MeshcatVisualizer(
+                self.robot.model, self.robot.collision_model, self.robot.visual_model)
+            self.viz.initViewer(open=True)
+        except ImportError as err:
+            print(
+                "Error while initializing the viewer. It seems you should install Python meshcat"
+            )
+            print(err)
+            sys.exit(0)
 
-        # for i in range(1, 6):
-        #     frame_name = "Foot_" + str(i)
-        #     self.robot.model.addFrame(
-        #         pin.Frame(frame_name, 1, 1)
-        #     )
-        print("DEBUG POINT")
+        # Load the robot in the viewer.
+        self.viz.loadViewerModel()
+
+        self.viz.displayFrames(visibility=True)
+        self.viz.displayCollisions(visibility=False)
+        self.viz.display(self.qc)
 
     def foot_pos_err(self, q, FRAME_ID=9, desired_pos=np.zeros(3)):
         self.robot.forwardKinematics(q)
@@ -69,13 +70,13 @@ class hexapod:
         for frame in self.robot.model.frames:
             # Print the frame name and type
             print(
-                f"Frame Name: {frame.name}, Frame Type: {frame.type}, Frame ID: {self.robot.model.getFrameId(frame.name)}, XYZ = {self.robot.framePlacement(self.robot.q0, self.robot.model.getFrameId(frame.name))}")
+                f"Frame Name: {frame.name}, Frame Type: {frame.type}, Frame ID: {self.robot.model.getFrameId(frame.name)}, XYZ = {self.robot.framePlacement(self.qc, self.robot.model.getFrameId(frame.name))}")
 
     def find_frame_info_by_name(self, name: str):
         for frame in self.robot.model.frames:
             if name in frame.name:
                 print(
-                    f"Frame Name: {frame.name}, Frame Type: {frame.type}, Frame ID: {self.robot.model.getFrameId(frame.name)}, XYZ = {self.robot.framePlacement(self.robot.q0, self.robot.model.getFrameId(frame.name))}")
+                    f"Frame Name: {frame.name}, Frame Type: {frame.type}, Frame ID: {self.robot.model.getFrameId(frame.name)}, XYZ = {self.robot.framePlacement(self.qc, self.robot.model.getFrameId(frame.name))}")
 
     def inverse_geometery(self, q, FRAME_ID=9, desired_pos=np.zeros(3)):
         bounds = [(0., 0.)]*self.robot.nq
@@ -88,27 +89,41 @@ class hexapod:
         bounds[6] = (1, 1)
         for i in range(7, self.robot.nq):
             bounds[i] = ((-np.pi/3), (np.pi/3))
-        # for i in range(7, self.robot.nq):
-        #     if i % 2 == 0:
-        #         bounds[i] = ((-np.pi/3), (np.pi/3))
-        #     else:
-        #         bounds[i] = (1, 1)
         res = scipy.optimize.minimize(
             self.foot_pos_err, q, args=(FRAME_ID, desired_pos), bounds=bounds)
         return res.x
 
     def north_vector(self):
-        print("North Vector")
         # Get frame of Joint_01
-        p1 = self.robot.framePlacement(self.robot.q0, 3).translation
+        p1 = self.robot.framePlacement(
+            self.qc, self.SHOULDER_IDS[0]).translation
         # Get frame for Joint_02
-        p2 = self.robot.framePlacement(self.robot.q0, 11).translation
+        p2 = self.robot.framePlacement(
+            self.qc, self.SHOULDER_IDS[1]).translation
         # find vector
-        v = np.array([-(p2 - p1)[1], (p2 - p1)[0]])
-        return v/(np.sqrt((v[0]**2) + (v[1]**2)))
+        v = np.array([(p2 - p1)[1], -(p2 - p1)[0]])
+        v = v/(np.sqrt((v[0]**2) + (v[1]**2)))
+        if self.viz_flag:
+            self.viz.viewer['direction_vector'].set_object(
+                g.Line(g.PointsGeometry(np.array(([0, 0, 0], np.hstack((v, [0.0])))).T
+                                        ), g.MeshBasicMaterial(color=0xffff00)))
+        return v
 
     def south_vector(self):
-        pass
+        # Get frame of Joint_01
+        p1 = self.robot.framePlacement(
+            self.qc, self.SHOULDER_IDS[3]).translation
+        # Get frame for Joint_02
+        p2 = self.robot.framePlacement(
+            self.qc, self.SHOULDER_IDS[4]).translation
+        # find vector
+        v = np.array([(p2 - p1)[1], -(p2 - p1)[0]])
+        v = v/(np.sqrt((v[0]**2) + (v[1]**2)))
+        if self.viz_flag:
+            self.viz.viewer['direction_vector'].set_object(
+                g.Line(g.PointsGeometry(np.array(([0, 0, 0], np.hstack((v, [0.0])))).T
+                                        ), g.MeshBasicMaterial(color=0xffff00)))
+        return v
 
     def east_vector(self):
         pass
@@ -145,18 +160,31 @@ class hexapod:
         }
         return slope_switch_dict.get(DIR, self.default_vector)()
 
-    def generate_path_function(self, DIR='N', FOOT_ID=9):
+    def init_foot_trajectory_functions(self, step_size_xy_mult, DIR='N', FOOT_ID=10):
         v = self.generate_direction_vector(DIR=DIR)
-        p1 = self.robot.framePlacement(self.robot.q0, FOOT_ID).translation[0:2]
-        p2 = p1 + (self.STEP_SIZE_XY * v)
-        print(f"v = {v}\np1 = {p1}\np2 = {p2}")
-        # def x_t(t): return ((1 - t) * p1[0]) + (t * p2[0])
+        p1 = self.robot.framePlacement(self.qc, FOOT_ID).translation[0:2]
+        p2 = p1 + (self.HALF_STEP_SIZE_XY * step_size_xy_mult * v)
         self.x_t = lambda t: ((1 - t) * p1[0]) + (t * p2[0])
         self.y_t = lambda t:  ((1 - t) * p1[1]) + (t * p2[1])
 
         self.z_t = lambda t: (((-self.STEP_SIZE_Z)/0.25) *
                               (t ** 2)) + ((self.STEP_SIZE_Z / 0.25) * t)
-        print("DEBUG POINT")
+        pass
+
+    def generate_joint_waypoints(self, step_size_xy_mult, STEPS=5, DIR='N', FOOT_ID=10):
+        self.init_foot_trajectory_functions(
+            step_size_xy_mult=step_size_xy_mult, DIR=DIR, FOOT_ID=FOOT_ID)
+        s = np.linspace(0, 1, STEPS)
+        waypoints = [[round(self.x_t(t), 5), round(
+            self.y_t(t), 5), round(self.z_t(t), 5)] for t in s]
+        points = np.array([waypoints[0], waypoints[4]]).T
+
+        if self.viz_flag:
+            self.viz.viewer[('Foot_ID_' + str(FOOT_ID) + '_trajectory')].set_object(
+                g.Line(g.PointsGeometry(points), g.MeshBasicMaterial(color=0xffff00)))
+
+        return [self.inverse_geometery(self.qc, FOOT_ID, wp)
+                for wp in waypoints]
 
     def compute_trajectory(self, position_init, position_goal, t_init, t_goal, t):
         t_tot = t_goal - t_init
@@ -175,62 +203,50 @@ class hexapod:
 
         return self.desired_position, self.desired_acceleration, self.desired_acceleration
 
+    def generate_leg_joint_trajectory(self, step_size_xy_mult, DIR='N', LEG=0, STEPS=5, t_init=0, t_goal=0.1, dt=0.01):
+        q_wps = self.generate_joint_waypoints(step_size_xy_mult,
+                                              DIR=DIR, FOOT_ID=self.FOOT_IDS[LEG], STEPS=STEPS)
+        q_traj = self.qc
+        mask = np.concatenate((np.zeros(6), [1], np.zeros(
+            LEG*3), [1, 1, 1], np.zeros((5-LEG)*3)))
+        for i in range(0, q_wps.__len__()-1):
+            t = t_init
+            while t < t_goal:
+                q_t = self.compute_trajectory(
+                    q_wps[i], q_wps[i+1], t_init, t_goal, t)[0]
+                q_traj = np.vstack((q_traj, np.multiply(q_t, mask)))
+                t = (t + dt)
+        return np.delete(q_traj, 0, axis=0)
+
 
 if __name__ == "__main__":
-    hexy = hexapod()
-
-    try:
-        # zmq_url = "tcp://127.0.0.1:7001"
-        # meshcat_vis = meshcat.Visualizer(zmq_url=zmq_url)
-        viz = pin.visualize.MeshcatVisualizer(
-            hexy.robot.model, hexy.robot.collision_model, hexy.robot.visual_model)
-        viz.initViewer(open=True)
-        # viz.initViewer(viewer=meshcat_vis, open=True)
-        # model.apply_colors(viz)
-    except ImportError as err:
-        print(
-            "Error while initializing the viewer. It seems you should install Python meshcat"
-        )
-        print(err)
-        sys.exit(0)
-
-    # Load the robot in the viewer.
-    viz.loadViewerModel()
-
-    viz.displayFrames(visibility=True)
-    viz.displayCollisions(visibility=False)
-
-    q0 = pin.neutral(hexy.robot.model)
-    viz.display(q0)
-
-    # des_pos = hexy.robot.framePlacement(
-    #     hexy.robot.q0, 10).translation + np.array([0, 0, 0.025])
-    # result = hexy.inverse_geometery(10, des_pos)
-    # # hexy.robot.forwardKinematics(result)
-    # print(
-    #     f"Desired Pos = {des_pos} \nCurrent Pos = {hexy.robot.framePlacement(result, 10).translation}")
-    # viz.display(result)
-
-    hexy.generate_path_function()
-    t_vals = np.linspace(0, 1, 5)
-    # waypoints = [[round(hexy.x_t(t), 5), round(
-    #     hexy.y_t(t), 5), round(hexy.z_t(t), 5)] for t in t_vals]
-    waypoints = [[hexy.x_t(t), hexy.y_t(t), hexy.z_t(t)] for t in t_vals]
-    # q = hexy.robot.q0
-    # for wp in waypoints:
-    #     q = hexy.inverse_geometery(q, 10, wp)
-    #     viz.display(q)
-    #     print(f"Current Pos = {hexy.robot.framePlacement(q, 10).translation}")
-    # sleep(0.5)
-    q_wps = [hexy.inverse_geometery(hexy.robot.q0, 10, wp) for wp in waypoints]
-    t_init = 0
-    t_goal = 1/10
-    dt = 0.01
-
-    for i in range(0, q_wps.__len__()-1):
-        t = t_init
-        while t < t_goal:
-            viz.display(hexy.compute_trajectory(
-                q_wps[i], q_wps[i+1], t_init, t_goal, t)[0])
-            t = round((t + dt), 2)
-    print("DEBUG POINT")
+    hexy = hexapod(init_viz=True)
+    sleep(3)
+    step_size_xy_mult = 1
+    leg0_traj = hexy.generate_leg_joint_trajectory(
+        step_size_xy_mult=step_size_xy_mult, DIR='N', LEG=0)
+    leg2_traj = hexy.generate_leg_joint_trajectory(
+        step_size_xy_mult=step_size_xy_mult, DIR='N', LEG=2)
+    leg4_traj = hexy.generate_leg_joint_trajectory(
+        step_size_xy_mult=step_size_xy_mult, DIR='N', LEG=4)
+    leg024_traj = leg0_traj + leg2_traj + leg4_traj
+    leg024_traj[:, 6] = 1
+    for q in leg024_traj:
+        hexy.viz.display(q)
+        hexy.qc = q
+    leg0_traj = None
+    leg2_traj = None
+    leg4_traj = None
+    sleep(3)
+    step_size_xy_mult = 2
+    leg0_traj = hexy.generate_leg_joint_trajectory(
+        step_size_xy_mult=step_size_xy_mult, DIR='S', LEG=0)
+    leg2_traj = hexy.generate_leg_joint_trajectory(
+        step_size_xy_mult=step_size_xy_mult, DIR='S', LEG=2)
+    leg4_traj = hexy.generate_leg_joint_trajectory(
+        step_size_xy_mult=step_size_xy_mult, DIR='S', LEG=4)
+    leg024_traj = leg0_traj + leg2_traj + leg4_traj
+    leg024_traj[:, 6] = 1
+    for q in leg024_traj:
+        hexy.viz.display(q)
+        hexy.qc = q
